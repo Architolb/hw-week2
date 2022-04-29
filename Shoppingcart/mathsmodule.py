fc376aaef67a297c4c0fe4f031e36a3080391368
Architolb <brandon_tolbert@outlook.com>
2022-04-28 23:08:46 -0500

    commit

---
+++ Shoppingcart/mathsmodule.py
@@ -0,0 +1,26 @@
+#square footage of house function for module1/2
+# from operator import length_hint
+def square_footage(length, width):
+    A = length * width
+    return A
+
+
+
+
+
+
+
+
+
+
+# circumference of a circle for module 2/2
+import math
+
+def circumf(radius):
+    pi = math.pi
+    circumference = 2 * (pi *radius)
+    print("The circumference will be", circumference)
+
+    
+
+
